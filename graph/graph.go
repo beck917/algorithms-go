25ad4b184fb7775e17085267f9ac9b454860b57f
XuPeng <xupeng@qutoutiao.net>
2020-10-26 20:43:34 +0800

    tmp

--- graph/graph.go
+++ graph/graph.go
@@ -25,17 +25,26 @@ func (graph *Graph) addEdge(s int, t int) {
 	graph.adj[t].PushBack(s)
 }
 
-var paths []int
-var passed map[int]bool
+var nodePath *Node
+var visited map[int]bool
 var found bool
+
+// Node 用链表存储遍历过的节点, 这样在回溯的时候,可以将next覆盖掉
+type Node struct {
+	next  *Node
+	value int
+}
 
 // DFS test
 func (graph *Graph) DFS(s int, t int) {
+	nodePath := &Node{
+		value: s,
+	}
 	graph.recurDfs(s, t)
 }
 
 func (graph *Graph) recurDfs(s int, t int) {
-	if passed[
+	visited[node.Value.(int)] = true
 
 	for node := graph.adj[s].Front(); node != nil; node = node.Next() {
 		if passed[node.Value.(int)] != true {
@@ -50,7 +59,11 @@ func (graph *Graph) recurDfs(s int, t int) {
 			paths = append(paths, node.Value.(int))
 			return
 		}
-		recurDfs(node.Value.(int), t)
+		if !visited[node.Value.(int)] {
+
+			recurDfs(node.Value.(int), t)
+		}
+
 	}
-	
+
 }
