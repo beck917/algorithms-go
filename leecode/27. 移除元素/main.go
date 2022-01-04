70aaa5a73445cbffdf9710aed8f1ce6a42c6ce1c
XuPeng <xupeng@qutoutiao.net>
2022-01-04 19:27:12 +0800

    leetcode

---
+++ leecode/27. 移除元素/main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+
+}
+
+func removeElement(nums []int, val int) int {
+	j := 0
+	count := len(nums)
+	for i := 0; i < count; i++ {
+		if nums[i] != val {
+			nums[i], nums[j] = nums[j], nums[i]
+			j++
+		}
+	}
+
+	return count - j
+}
---
+++ leecode/977. 有序数组的平方/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{-5, -1, 0, 1, 3}
+	fmt.Println(sortedSquares(nums))
+}
+
+func sortedSquares(nums []int) []int {
+
+}
