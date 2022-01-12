dee308b02c04d1d9a9df27194357a90b2a73f13e
XuPeng <xupeng@qutoutiao.net>
2022-01-12 18:49:14 +0800

    tmp

--- leecode/142.环形链表II/main.go
+++ leecode/142.环形链表II/main.go
@@ -9,12 +9,15 @@ func detectCycle(head *ListNode) *ListNode {
 	fast := head
 	slow := head
 
-	for fast != nil {
+	for fast != nil && fast.Next != nil {
 		fast = fast.Next.Next
 		slow = slow.Next
 
+		// 需要公式推到，非人类思维可以直接理解。。。
 		if fast == slow {
-
+			for slow.Next
 		}
 	}
+
+	return false
 }
