decfe265de38ea2e73d32fcaac16afbd485f6406
XuPeng <xupeng@qutoutiao.net>
2022-01-12 17:41:08 +0800

    leetcode

--- leecode/141. 环形链表/main.go
+++ leecode/141. 环形链表/main.go
@@ -5,16 +5,18 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func detectCycle(head *ListNode) *ListNode {
+func hasCycle(head *ListNode) bool {
 	fast := head
 	slow := head
 
-	for fast != nil {
+	for fast != nil && fast.Next != nil {
 		fast = fast.Next.Next
 		slow = slow.Next
 
 		if fast == slow {
-
+			return true
 		}
 	}
+
+	return false
 }
--- leecode/344. 反转字符串/main.go
+++ leecode/344. 反转字符串/main.go
@@ -8,11 +8,22 @@ func main() {
 	fmt.Println(s)
 }
 
-func reverseString(s []byte) {
+func reverseString2(s []byte) {
 	slen := len(s)
 	j := slen - 1
 	for i := 0; i < slen/2; i++ {
 		s[i], s[j] = s[j], s[i]
 		j--
+	}
+}
+
+func reverseString(s []byte) {
+	slen := len(s)
+	right := slen - 1
+	left := 0
+	for left < right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
 	}
 }
