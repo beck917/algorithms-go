d0697b61cff192dbffdb691a2d75054e9312377b
XuPeng <xupeng@qutoutiao.net>
2022-03-03 15:57:34 +0800

    dp

--- leetcode/1143. 最长公共子序列/main.go
+++ leetcode/1143. 最长公共子序列/main.go
@@ -1,5 +1,51 @@
 package main
 
-func longestCommonSubsequence(text1 string, text2 string) int {
+import (
+	"fmt"
+)
 
+func main() {
+	fmt.Println(longestCommonSubsequence("abcde", "ace"))
+}
+
+// 输入：text1 = "abcde", text2 = "ace" z
+// "bsbininm" "jmjkbkjkv"
+func longestCommonSubsequence(text1 string, text2 string) int {
+	dp := make([][]int, len(text1))
+
+	tmpi := len(text1)
+	for i := 0; i < len(text1); i++ {
+		dp[i] = make([]int, len(text2))
+		if text1[i] == text2[0] || i > tmpi {
+			dp[i][0] = 1
+			tmpi = i
+		}
+	}
+
+	tmpi = len(text2)
+	for i := 0; i < len(text2); i++ {
+
+		if text1[0] == text2[i] || i > tmpi {
+			dp[0][i] = 1
+			tmpi = i
+		}
+	}
+
+	for i := 1; i < len(text1); i++ {
+		for j := 1; j < len(text2); j++ {
+			tmp := dp[i-1][j]
+			if text1[i] == text2[j] {
+				dp[i][j] = dp[i-1][j-1] + 1
+				fmt.Println(i, j, dp[i][j], string(text1[i]), string(text2[j]))
+			} else {
+				if dp[i][j-1] > tmp {
+					tmp = dp[i][j-1]
+				}
+				dp[i][j] = tmp
+			}
+			//fmt.Println(i, j, dp[i][j])
+		}
+	}
+
+	return dp[len(text1)-1][len(text2)-1]
 }
