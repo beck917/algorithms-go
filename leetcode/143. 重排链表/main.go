1086581c12401f5a2d55578266e30f1f095b8c14
XuPeng <xupeng@qutoutiao.net>
2022-03-09 13:33:23 +0800

    优化重排链表

--- leetcode/143. 重排链表/main.go
+++ leetcode/143. 重排链表/main.go
@@ -5,11 +5,59 @@ type ListNode struct {
 	Next *ListNode
 }
 
+func reorderList(head *ListNode) {
+	fast := head
+	slow := head
+	i := 0
+	// 获取链表中间节点
+	for fast != nil {
+		fast = fast.Next
+		if i%2 == 0 {
+			slow = slow.Next
+		}
+		i++
+	}
+
+	mid := slow
+	left := head
+	right := mid.Next
+
+	// 反转右边链表
+	right = reverse(right)
+
+	// 合并链表
+	cur := left
+	for cur != mid {
+		next := cur.Next
+		rightnext := right.Next
+		cur.Next = right
+		right.Next = next
+
+		cur = next
+		right = rightnext
+	}
+	mid.Next = nil
+}
+
+func reverse(head *ListNode) *ListNode {
+	cur := head
+	var next, prev *ListNode
+
+	for cur != nil {
+		next = cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+
+	return prev
+}
+
 /**
 输入：head = [1,2,3,4,5]
 输出：[1,5,2,4,3]
 */
-func reorderList(head *ListNode) {
+func reorderList2(head *ListNode) {
 	if head.Next == nil || head.Next.Next == nil {
 		return
 	}
