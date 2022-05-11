bdfb80b8de5a04b01a757bc07efe399706cd0c69
XuPeng <xupeng@qutoutiao.net>
2022-05-11 17:00:58 +0800

    leetcode

--- leetcode/230. 二叉搜索树中第K小的元素/main.go
+++ leetcode/230. 二叉搜索树中第K小的元素/main.go
@@ -6,6 +6,29 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+var stack []int
+
 func kthSmallest(root *TreeNode, k int) int {
-	return 0
+	stack = make([]int, 0)
+	dfs(root, k)
+
+	if len(stack) < k {
+		return -1
+	}
+
+	return stack[len(stack)-1]
+}
+
+func dfs(root *TreeNode, k int) {
+	if root == nil {
+		return
+	}
+
+	dfs(root.Left, k)
+	if len(stack) == k {
+		return
+	}
+	stack = append(stack, root.Val)
+	dfs(root.Right, k)
+
 }
